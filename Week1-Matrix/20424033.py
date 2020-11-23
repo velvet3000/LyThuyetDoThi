00071b739f3f94ed843e93f59caf62f3780b36ef
velvet3000 <nmhoang.17ck@gmail.com>
2020-11-23 11:32:27 +0700

    add direct Graph

--- Week1-Matrix/20424033.py
+++ Week1-Matrix/20424033.py
@@ -5,39 +5,26 @@ import numpy as np
 
 def main():
    arr = readFile('data.txt')
-   print("Do thi: ")
-   for line in arr:
-       print (line)
-
-   if cau1(arr):
+   print(arr)
+   print("Cau 1: ")
+   if isUnDirectGraph(arr):
        print ("Là đồ thị vô hướng")
    else:
        print ("Là đồ thị có hướng")
-
-   # cau2
    print("Câu 2:\n Danh sách đỉnh kề: ", *cau2(arr))
-   # Cau3
    print("Câu 3:\nDanh sách bậc của từng đỉnh theo thứ tự đỉnh: ", *cau3(arr))
-   # Cau4  
-   print("Câu 4:\nSố lượng đỉnh bậc chẵn: ", cau4(arr))
-   # Cau5
-   print("Câu 5\nSố lượng đỉnh bậc lẻ: ", cau5(arr))
-   # Cau6 
-   print("Câu 6:\nSố lượng đỉnh treo: ", cau6(arr))
-   # Cau7 
-   print("Câu 7:\nSố lượng đỉnh cô lập: ", cau7(arr))
+   levelArray = cau3(arr)
+   print("Câu 4:\nSố lượng đỉnh bậc chẵn: ", cau4(levelArray))
+   print("Câu 5\nSố lượng đỉnh bậc lẻ: ", cau5(levelArray))
+   print("Câu 6:\nSố lượng đỉnh treo: ", cau6(levelArray))
+   print("Câu 7:\nSố lượng đỉnh cô lập: ", cau7(levelArray))
 
 def readFile(path):
-    arr = []
-    for line in open(path, 'r'):
-        arr.append(list(map(int,line.rstrip("\n").split(" "))))
-    return arr
+    data = np.loadtxt(path)
+    return data
 
-def cau1(arr):
-    print("\nCâu 1: ")
-    i = 0
+def isUnDirectGraph(arr):
     for i in range(len(arr)):
-        j = 0
         for j in range(len(arr)):
             if arr[i][j] != arr[j][i]:
                 return False
@@ -55,33 +42,34 @@ def cau2(arr):
 
 def cau3(arr):
     result = []
-    for line in arr:
-        result.append(sum(map(lambda i: i > 0, line)))
+    if(isUnDirectGraph(arr)):
+        for line in arr:
+            result.append(sum(map(lambda i: i > 0, line)))
+    else:
+        i = 0
+        total = 0
+        for i in range(len(arr)):
+            j = 0
+            for j in range(len(arr)):
+                if arr[i][j] > 0:
+                    total += 1
+                if arr[j][i] > 0:
+                    total += 1
+            result.append(total)
+            total = 0
     return result
 
+
 def cau4(arr):
-    count=0
-    for line in arr:
-        count += sum(map(lambda i: i % 2 == 0 and i > 0, line))
-    return count
+    return sum(map(lambda i: i % 2 == 0 and i > 0, arr))
 
 def cau5(arr):
-    count=0
-    for line in arr:
-        count += sum(map(lambda i: i % 2 != 0, line))
-    return count
+    return sum(map(lambda i: i % 2 != 0, arr))
 
 def cau6(arr):
-    count=0
-    for line in arr:
-        count += sum(map(lambda i: i == 1, line))
-    return count
+    return sum(map(lambda i: i == 1, arr))
 
 def cau7(arr):
-    count=0
-    for line in arr:
-        count += sum(map(lambda i: i == 0, line))
-    return count
-    
+    return sum(map(lambda i: i == 0, arr))
 
 main()
